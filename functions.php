5d29358ad805692366ad77fd6daeee966ba44415
Numan Hussain <ahnuman5@gmail.com>
2020-03-06 22:53:50 +0600

    added custom header function

--- functions.php
+++ functions.php
@@ -6,12 +6,24 @@
           define("VERSION", wp_get_theme()->get("VERSION"));
      }
 
-     function devuteAlpha_after_done(){
+     function wpNewsPortal_after_done(){
           load_theme_textdomain("wpNewsPortal"); 
           add_theme_support("post-thumbnails"); 
           add_theme_support("title-tag");                   
           add_theme_support("custom-background");      
-          add_theme_support( "custom-header" );            
+          add_theme_support( "custom-header" );     
+         
+         
+          $wpNewsPortal_custom_header_details = array(
+               'header-text'         => true,
+               'default-text-color'  => '#dd9933',
+               'width'               => 1200,
+               'height'              => 600,
+               'flex-height'         => true,
+               'flex-width'          => true,
+       
+           );
+           add_theme_support("custom-header", $_custom_header_details);       
 
      }
 
